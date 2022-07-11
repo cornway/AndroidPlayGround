3d077e7ce9928526b50a6fa7bef8fdc1973a1d51
roman pustobaiev <roman.pustobayev@trackimo.com>
2022-07-11 17:19:59 +0300

    Adding junk data

--- github/src/main/java/com/github/data/offline/RequestDataRepositoriesImpl.kt
+++ github/src/main/java/com/github/data/offline/RequestDataRepositoriesImpl.kt
@@ -7,12 +7,13 @@ class RequestDataRepositoriesOfflineImpl : RequestDataRepositories {
     override suspend fun requestPerUser(userName: String): Array<Repositories>? {
         val owner = Repositories.Owner(login = "NoName")
         val repo = Repositories(name = "NoName", fullName = "NoFullName", owner = owner)
-        return arrayOf(repo , repo , repo)
+        return Array(20){repo}
     }
 
     override suspend fun requestFeed(since: Int, perPage: Int): Array<Repositories>? {
-        val owner = Repositories.Owner(login = "NoName")
-        val repo = Repositories(name = "NoName", fullName = "NoFullName", owner = owner)
-        return arrayOf(repo , repo , repo)
+        val owner = Repositories.Owner(login = "NoName : ")
+        return Array(20){i -> Repositories(name = "NoName", fullName = "NoFullName", owner = owner).apply {
+            fullName += (i+since).toString()
+        }}
     }
 }
